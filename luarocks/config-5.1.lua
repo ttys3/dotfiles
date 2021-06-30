92afe26d5ec4f98617586f38e8b79b0cf2834802
荒野無燈 <ttys3@outlook.com>
2021-07-01 00:35:42 +0800

    add config-5.1.lua for luarocks

---
+++ luarocks/config-5.1.lua
@@ -0,0 +1,15 @@
+lib_modules_path = "/lib64/lua/5.1"
+-- lua_modules_path = "/usr/share/lua/5.1"
+local_by_default = true
+lua_version = "5.1"
+lua_interpreter = "lua5.1";
+rocks_dir = "/home/ttys3/.luarocks/lib/luarocks/rocks-5.1"
+deploy_lua_dir = "/home/ttys3/.luarocks/share/lua/5.1"
+
+variables = {
+    LUA = "/usr/bin/lua5.1";
+    LUA_DIR = "/usr";
+    LUA_BINDIR = "/usr/bin";
+    ROCKS_TREE = "/home/ttys3/.luarocks/lib/luarocks/rocks-5.1";
+ }
+ 
