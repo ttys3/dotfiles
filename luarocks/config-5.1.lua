c1861207617ddaa0c1e7542cf4edc1a394b76cfd
荒野無燈 <ttys3@outlook.com>
2021-07-01 01:05:53 +0800

    fix: fixup config-5.1.lua

--- luarocks/config-5.1.lua
+++ luarocks/config-5.1.lua
@@ -9,16 +9,35 @@
 
 lua_version = "5.1"
 
-lua_modules_path = "/usr/share/lua/"..lua_version
-lib_modules_path = "/lib64/lua/"..lua_version
-
-rocks_subdir = "/usr/lib/luarocks/rocks-"..lua_version,
-
-connection_timeout = 30,  -- 0 = no timeout
-
 local_by_default = true
 
 lua_interpreter = "lua5.1";
+
+-- do not prepend `/usr` to lua_modules_path or lib_modules_path
+-- no need to change the default generally.
+-- otherwise both `deploy_lib_dir` and `deploy_lua_dir` value will be wrong
+----------------------------------------------------------------
+-- remember: 
+-- `home_tree = "/home/ttys3/.luarocks"`
+-- `deploy_lua_dir = home_tree .. lua_modules_path`
+-- `deploy_lib_dir = home_tree .. lib_modules_path`
+----------------------------------------------------------------
+-- lua_modules_path = "/share/lua/"..lua_version
+-- lib_modules_path = "/lib/lua/"..lua_version
+
+-- we need correct `lib_modules_path` here
+lib_modules_path = "/lib64/lua/"..lua_version
+
+-- for 5.1 ROCKS_TREE is `/home/ttys3/.luarocks/lib/luarocks/rocks-5.1`
+-- so rocks_subdir is `lib/luarocks/rocks-5.1`, no need to change the default
+-- set it to full absolute path is incorrect, it does not need the `~/.luarocks` part
+-- otherwise both `variables.ROCKS_TREE` and `rocks_dir` value will be wrong
+-- remember:  `rocks_dir = home_tree .. rocks_subdir`
+-- system: /usr/lib/luarocks/rocks-5.1
+-- user: /home/ttys3/.luarocks/lib/luarocks/rocks-5.1
+-- rocks_subdir = "/lib/luarocks/rocks-"..lua_version
+
+connection_timeout = 30  -- 0 = no timeout
 
 variables = {
     LUA = "/usr/bin/lua5.1";
