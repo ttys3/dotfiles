785ba51d49d4b57c3283023e1414fd51c63e310d
荒野無燈 <ttys3@outlook.com>
2021-07-01 01:07:50 +0800

    chore: add sample `luarocks config` output

---
+++ luarocks/config.sample.arch.lua
@@ -0,0 +1,187 @@
+accept_unknown_fields = false
+arch = "linux-x86_64"
+cache = {
+   luajit_version_checked = true
+}
+cache_fail_timeout = 86400
+cache_timeout = 60
+check_certificates = false
+cmake_generator = "Unix Makefiles"
+config_files = {
+   nearest = "/home/ttys3/.luarocks/config-5.1.lua",
+   system = {
+      file = "/etc/luarocks/config-5.4.lua",
+      found = true
+   },
+   user = {
+      file = "/home/ttys3/.luarocks/config-5.1.lua",
+      found = true
+   }
+}
+connection_timeout = 30
+deploy_bin_dir = "/home/ttys3/.luarocks/bin"
+deploy_lib_dir = "/home/ttys3/.luarocks/lib64/lua/5.1"
+deploy_lua_dir = "/home/ttys3/.luarocks/share/lua/5.1"
+deps_mode = "one"
+disabled_servers = {}
+export_path_separator = ":"
+external_deps_dirs = {
+   "/usr/local",
+   "/usr",
+   "/"
+}
+external_deps_patterns = {
+   bin = {
+      "?"
+   },
+   include = {
+      "?.h"
+   },
+   lib = {
+      "lib?.a",
+      "lib?.so",
+      "lib?.so.*"
+   }
+}
+external_deps_subdirs = {
+   bin = "bin",
+   include = "include",
+   lib = {
+      "lib",
+      "lib64"
+   }
+}
+external_lib_extension = "so"
+fs_use_modules = true
+gcc_rpath = true
+home = "/home/ttys3"
+home_tree = "/home/ttys3/.luarocks"
+hooks_enabled = true
+lib_extension = "so"
+lib_modules_path = "/lib64/lua/5.1"
+link_lua_explicitly = false
+local_by_default = true
+local_cache = "/home/ttys3/.cache/luarocks"
+lua_extension = "lua"
+lua_found = {
+   lua_bindir = "/usr/bin",
+   lua_dir = "/usr",
+   lua_interpreter = "lua5.1",
+   lua_version = "5.1"
+}
+lua_interpreter = "lua5.1"
+lua_modules_path = "/share/lua/5.1"
+lua_version = "5.1"
+major_version = "3.7"
+no_manifest = false
+obj_extension = "o"
+processor = "x86_64"
+program_series = "3.7"
+program_version = "3.7.0"
+rocks_dir = "/home/ttys3/.luarocks/lib/luarocks/rocks-5.1"
+rocks_servers = {
+   {
+      "https://luarocks.org",
+      "https://raw.githubusercontent.com/rocks-moonscript-org/moonrocks-mirror/master/",
+      "https://luafr.org/luarocks/",
+      "http://luarocks.logiceditor.com/rocks"
+   }
+}
+rocks_subdir = "/lib/luarocks/rocks-5.1"
+rocks_trees = {
+   {
+      name = "user",
+      root = "/home/ttys3/.luarocks"
+   },
+   {
+      name = "system",
+      root = "/usr"
+   }
+}
+runtime_external_deps_patterns = {
+   bin = {
+      "?"
+   },
+   include = {
+      "?.h"
+   },
+   lib = {
+      "lib?.so",
+      "lib?.so.*"
+   }
+}
+runtime_external_deps_subdirs = {
+   bin = "bin",
+   include = "include",
+   lib = {
+      "lib",
+      "lib64"
+   }
+}
+static_lib_extension = "a"
+sysconfdir = "/etc/luarocks"
+target_cpu = "x86_64"
+upload = {
+   api_version = "1",
+   server = "https://luarocks.org",
+   tool_version = "1.0.0"
+}
+user_agent = "LuaRocks/3.7.0 linux-x86_64"
+variables = {
+   AR = "ar",
+   BUNZIP2 = "bunzip2",
+   CC = "gcc",
+   CFLAGS = "-O2 -fPIC",
+   CHMOD = "chmod",
+   CMAKE = "cmake",
+   CP = "cp",
+   CURL = "curl",
+   CURLNOCERTFLAG = "-k",
+   CVS = "cvs",
+   FIND = "find",
+   GIT = "git",
+   GPG = "gpg",
+   GUNZIP = "gunzip",
+   HG = "hg",
+   ICACLS = "icacls",
+   LD = "gcc",
+   LIBFLAG = "-shared",
+   LIB_EXTENSION = "so",
+   LS = "ls",
+   LUA = "/usr/bin/lua5.1",
+   LUALIB = "liblua.so",
+   LUA_BINDIR = "/usr/bin",
+   LUA_DIR = "/usr",
+   LUA_INCDIR = "/usr/include/lua5.1",
+   LUA_LIBDIR = "/usr/lib",
+   LUA_LIBDIR_FILE = "liblua.so",
+   MAKE = "make",
+   MD5 = "md5",
+   MD5SUM = "md5sum",
+   MKDIR = "mkdir",
+   MKTEMP = "mktemp",
+   OBJ_EXTENSION = "o",
+   OPENSSL = "openssl",
+   PWD = "pwd",
+   RANLIB = "ranlib",
+   RM = "rm",
+   RMDIR = "rmdir",
+   ROCKS_TREE = "/home/ttys3/.luarocks/lib/luarocks/rocks-5.1",
+   RSYNC = "rsync",
+   RSYNCFLAGS = "--exclude=.git -Oavz",
+   SCP = "scp",
+   SCRIPTS_DIR = "/home/ttys3/.luarocks/bin",
+   SEVENZ = "7z",
+   SSCM = "sscm",
+   SVN = "svn",
+   TAR = "tar",
+   TEST = "test",
+   TOUCH = "touch",
+   UNZIP = "unzip -n",
+   WGET = "wget",
+   WGETNOCERTFLAG = "--no-check-certificate",
+   ZIP = "zip"
+}
+web_browser = "xdg-open"
+wrapper_suffix = ""
+
